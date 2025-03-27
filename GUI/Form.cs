a5bb68114a262b2be3f1a00a4013a35fbd445e67
RodrigoDev <RodrigoDev@DESKTOP-KAPAARJ>
2025-03-26 22:18:20 -0300

    ajuste using

--- GUI/Form.cs
+++ GUI/Form.cs
@@ -1,8 +1,9 @@
 using System.Text;
 using Manco;
 using AssemblerSimulator;
-using Manco.Common.Exceptions;
 using Manco.Common.Enums;
+using Manco.Common.Wrapper;
+using Manco.Common.Exceptions;
 using Timer = System.Windows.Forms.Timer;
 using WinForm = System.Windows.Forms.Form;
 
